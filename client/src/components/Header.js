fb75e2795dc275eb0ddf43ca05ca60a4ce1e559a
Miah Barnes <miahellenbarnes@gmail.com>
2020-11-23 18:17:07 -0500

    skeleton

---
+++ client/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+const Header = () => {
+
+    return (
+        <div className='header'>
+            <div className='image-title-box'>
+                <div className='profile-picture'></div>
+                <div>Miah Barnes</div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Header;

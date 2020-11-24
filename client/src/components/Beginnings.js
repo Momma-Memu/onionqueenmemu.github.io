657e44b7b0e2e34e8fa6ae692dcb27a0cefcbe74
Miah Barnes <miahellenbarnes@gmail.com>
2020-11-24 17:35:15 -0500

    yur mum

--- client/src/components/Beginnings.js
+++ client/src/components/Beginnings.js
@@ -12,7 +12,13 @@ const Beginnings = () => {
                 <div className='fing-quote'>Don't be afraid of failure, failure is your greatest teacher.</div>
             </div>
             <div className='sliding-story' ref={ani}>
-
+                <div className='starting-message'>I started my education in software engineering by diving head first.</div>
+                <i className="fas fa-arrow-down down-arrow"></i>
+                <div className='arrow-dates'>April 15, 2020</div>
+                <div className='edx-part'>I took a CS50 course hosted by EDX and Harvard. It was this course that made me fall in love with code.</div>
+                <i className="fas fa-arrow-down down-arrow"></i>
+                <div className='arrow-dates'>June 15, 2020</div>
+                <div className='edx-part'>App Academy was my second stop. I made it through a 6 month long </div>
             </div>
         </div>
     )

4b7b01a5d3e9b75f42d8b8dcff63be8114b8b808
Miah Barnes <miahellenbarnes@gmail.com>
2020-11-23 19:53:40 -0500

    techbar

--- client/src/components/TechBar.js
+++ client/src/components/TechBar.js
@@ -3,6 +3,22 @@ import React from 'react';
 
 const TechBar = () => {
 
+    // const logos = [
+    //     <div className='html-logo'></div>,
+    //     <div className='css-logo'></div>,
+    //     <div className='js-logo'></div>,
+    //     <div className='mui-logo'></div>,
+    //     <div className='pgs-logo'></div>,
+    //     <div className='sqlize-logo'></div>,
+    //     <div className='react-logo'></div>,
+    //     <div className='python-logo'></div>,
+    //     <div className='flask-logo'></div>,
+    // ];
+
+    // const slideShow = () => {
+
+    // }
+
     return (
         <div className='tech-bar-wrapper'>
             <div className='html-logo'></div>

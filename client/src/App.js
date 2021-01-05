0cae77ef3685b64fe211a7d7f66413de3dcc71b4
Miah Barnes <miahellenbarnes@gmail.com>
2021-01-05 11:53:08 -0500

    link fix

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import TechBar from './components/TechBar';
 import Begginnings from './components/Beginnings';
 import Projects from './components/Projects';
+import Descriptions from './components/Descriptions';
 // import Vidalia from './components/Vidalia';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       </header>
       <Begginnings />
       <Projects />
+      <Descriptions />
     </div>
   );
 }
---
+++ client/src/components/Descriptions.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+const Descriptions = () => {
+
+    return (
+        <div className='divider'>Project information: </div>
+    )
+}
+
+
+export default Descriptions;

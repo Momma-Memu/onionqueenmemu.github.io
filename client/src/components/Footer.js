b03f2933ceaaed778fbd771e963e88a8c82b704d
Miah Barnes <miahellenbarnes@gmail.com>
2021-01-05 18:51:37 -0500

    email link

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -9,6 +9,7 @@ const Footer = () => {
             <a href='https://www.linkedin.com/in/miah-barnes-2260111aa/' className='contact-linked'><i className="fab fa-linkedin"></i></a>
             <a href='https://github.com/OnionQueenMemu' className='contact-links'><i className="fab fa-github-square"></i></a>
             <a href='https://twitter.com/OnionQueenMemu' className='contact-twitter'><i className="fab fa-twitter-square"></i></a>
+            <a class="social-list__link" href="mailto:miahellenbarnes@gmail.com" className='contact-email'><i className="fas fa-envelope-square"></i></a>
         </div>
 
     )

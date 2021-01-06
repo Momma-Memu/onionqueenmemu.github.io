344a6793d64d9d58c9cff9df644c8d109ab29345
Miah Barnes <miahellenbarnes@gmail.com>
2021-01-06 18:12:19 -0500

    fix mobile use

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -6,15 +6,15 @@ const Projects = () => {
     return (
         <div className='projects-wrapper'>
             <div className='projects-header'>
-                <a href='http://vidalia.herokuapp.com/login' target='new' className='project-title-v'>Vidalia
-                    <div className='project-background-v'></div>
-                </a>
-                <a href='https://jellynettle.herokuapp.com/login' target='new' className='project-title-j'>JellyNettle
-                    <div className='project-background-j'></div>
-                </a>
-                <a href='http://whilesingle.herokuapp.com' className='project-title-w'>While(Single):
-                    <div className='project-background-w'></div>
-                </a>
+                <div className='project-title-v'>Vidalia
+                    <a href='http://vidalia.herokuapp.com/login' target='new'><div className='project-background-v'></div></a>
+                </div>
+                <div className='project-title-j'>JellyNettle
+                    <a href='https://jellynettle.herokuapp.com/login' target='new'><div className='project-background-j'></div></a>
+                </div>
+                <div className='project-title-w'>While(Single):
+                    <a href='http://whilesingle.herokuapp.com' target='new'><div className='project-background-w'></div></a>
+                </div>
             </div>
         </div>
     )

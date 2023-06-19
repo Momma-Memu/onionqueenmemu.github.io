ee331992bf9360b4d70e5ede667bc55a049f8062
Miah Barnes <miahbarnes@ziftsolutions.com>
2023-06-19 16:27:54 -0400

    update to borders, tech list, and mobile card stack

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,21 +1,17 @@
 import React from 'react';
-// const resume = require('../Miah_Barnes_resume.pdf');
 
 
 const Footer = () => {
-
-    // console.log(resume)
 
     return (
         <div className='footer-divider'>
             <div className='contact-me'>Contact Me</div>
             <a href='https://www.linkedin.com/in/miah-barnes-2260111aa/' target='new' className='contact-linked'><i className="fab fa-linkedin"></i></a>
             <a href='https://github.com/OnionQueenMemu' target='new' className='contact-links'><i className="fab fa-github-square"></i></a>
-            <a href='https://twitter.com/OnionQueenMemu' target='new' className='contact-twitter'><i className="fab fa-twitter-square"></i></a>
-            <a href='https://drive.google.com/file/d/1m9JanZjsQYbpNPPQ25FyD3BP7Ddh2Ktb/view?usp=sharing' className='resume'><i className="far fa-file"></i></a>
+            <a href='https://twitter.com/MommaMemu' target='new' className='contact-twitter'><i className="fab fa-twitter-square"></i></a>
+            <a href='https://drive.google.com/file/d/1CvrS-KG2xZ8fROp4vayq6qpFECSCBVKQ/view?usp=sharing' className='resume'><i className="fas fa-file"></i></a>
             <a className="social-list__link contact-email" href="mailto:miahellenbarnes@gmail.com"><i className="fas fa-envelope-square"></i></a>
         </div>
-
     )
 }
 
--- client/src/components/TechBar.js
+++ client/src/components/TechBar.js
@@ -5,16 +5,17 @@ const TechBar = () => {
 
     return (
         <div className='tech-bar-wrapper'>
-            <div className='html-logo'><span className='tooltiptext'>HTML</span></div>
-            <div className='css-logo'><span className='tooltiptext'>CSS</span></div>
-            <div className='js-logo'><span className='tooltiptext'>JavaScript</span></div>
-            <div className='mui-logo'><span className='tooltiptext'>Material-UI</span></div>
-            <div className='pgs-logo'><span className='tooltiptext'>PostgreSQL</span></div>
-            <div className='sqlize-logo'><span className='tooltiptext'>Sequelize</span></div>
-            <div className='react-logo'><span className='tooltiptext'>React</span></div>
-            <div className='python-logo'><span className='tooltiptext'>Python</span></div>
-            <div className='flask-logo'><span className='tooltiptext'>Flask</span></div>
-            {/* <div className='angular-logo'><span className='tooltiptext'>Angular</span></div> */}
+            <div className='html-logo tech-logo'><span className='tooltiptext'>HTML</span></div>
+            <div className='css-logo tech-logo'><span className='tooltiptext'>CSS</span></div>
+            <div className='js-logo tech-logo'><span className='tooltiptext'>JavaScript</span></div>
+            <div className='python-logo tech-logo'><span className='tooltiptext'>Python</span></div>
+            <div className='java-logo tech-logo'><span className='tooltiptext'>Java</span></div>
+            <div className='mui-logo tech-logo'><span className='tooltiptext'>Material-UI</span></div>
+            <div className='pgs-logo tech-logo'><span className='tooltiptext'>PostgreSQL</span></div>
+            <div className='sqlize-logo tech-logo'><span className='tooltiptext'>Sequelize</span></div>
+            <div className='react-logo tech-logo'><span className='tooltiptext'>React</span></div>
+            <div className='flask-logo tech-logo'><span className='tooltiptext'>Flask</span></div>
+            <div className='angular-logo tech-logo'><span className='tooltiptext'>Angular</span></div>
         </div>
     )
 }

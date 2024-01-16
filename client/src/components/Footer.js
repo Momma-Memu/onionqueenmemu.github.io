5b42f25823ffeb53778c0874b0e78de5d3657e86
Miah Brnes <miahbarnes@ziftsolutions.com>
2024-01-16 09:31:26 -0500

    update github url

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className='footer-divider'>
             <div className='contact-me'>Contact Me</div>
             <a href='https://www.linkedin.com/in/miah-barnes-2260111aa/' target='_blank' rel='noreferrer' className='contact-linked'><i className="fab fa-linkedin"></i></a>
-            <a href='https://github.com/OnionQueenMemu' target='_blank' rel='noreferrer' className='contact-links'><i className="fab fa-github-square"></i></a>
+            <a href='https://github.com/Momma-Memu' target='_blank' rel='noreferrer' className='contact-links'><i className="fab fa-github-square"></i></a>
             <a href='https://drive.google.com/file/d/1CvrS-KG2xZ8fROp4vayq6qpFECSCBVKQ/view?usp=sharing' target='_blank' rel='noreferrer' className='resume'><i className="fas fa-file"></i></a>
             <a className="social-list__link contact-email" href="mailto:miahellenbarnes@gmail.com"><i className="fas fa-envelope-square"></i></a>
         </div>
